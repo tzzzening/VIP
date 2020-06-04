a42abd787e8fc7a637f846dd51769794cfa63cec
chengTzeNing <09nhnccgtzening@gmail.com>
2020-06-04 12:06:42 +0800

    Switch from SimultaneousActivation to BaseScheduler

--- ABM/abm/model.py
+++ ABM/abm/model.py
@@ -1,5 +1,5 @@
 from mesa import Model
-from abm.time import SimultaneousActivationMoneyModel
+from abm.time import BaseSchedulerMoneyModel
 from abm.agents import Seller, Buyer
 
 
@@ -7,7 +7,7 @@ class MoneyModel(Model):
     def __init__(self, num_per_agent):
         super().__init__()
         self.num_per_agent = num_per_agent
-        self.schedule = SimultaneousActivationMoneyModel(self)
+        self.schedule = BaseSchedulerMoneyModel(self)
         for i in range(num_per_agent):
             seller = Seller(unique_id=self.next_id(), goods_left=5, min_price=5, model=self)
             buyer = Buyer(unique_id=self.next_id(), money_left=50, max_price=10, model=self)
