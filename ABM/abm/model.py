01fb33beec32c8386458d00e2ece8dbd80eb8a83
chengTzeNing <09nhnccgtzening@gmail.com>
2020-06-04 13:01:51 +0800

    Fix minor bug in conditional statement

--- ABM/abm/model.py
+++ ABM/abm/model.py
@@ -8,11 +8,19 @@ class MoneyModel(Model):
         super().__init__()
         self.num_per_agent = num_per_agent
         self.schedule = BaseSchedulerMoneyModel(self)
-        for i in range(num_per_agent):
-            seller = Seller(unique_id=self.next_id(), goods_left=5, min_price=5, model=self)
-            buyer = Buyer(unique_id=self.next_id(), money_left=50, max_price=10, model=self)
-            self.schedule.add(seller)
-            self.schedule.add(buyer)
+        # for i in range(num_per_agent):
+        #     seller = Seller(unique_id=self.next_id(), goods_left=5, min_price=5, model=self)
+        #     buyer = Buyer(unique_id=self.next_id(), money_left=50, max_price=10, model=self)
+        #     self.schedule.add(seller)
+        #     self.schedule.add(buyer)
+        seller = Seller(unique_id=self.next_id(), goods_left=5, min_price=5, model=self)
+        buyer = Buyer(unique_id=self.next_id(), money_left=5, max_price=5, model=self)
+        self.schedule.add(seller)
+        self.schedule.add(buyer)
+        seller = Seller(unique_id=self.next_id(), goods_left=5, min_price=5, model=self)
+        buyer = Buyer(unique_id=self.next_id(), money_left=50, max_price=6, model=self)
+        self.schedule.add(seller)
+        self.schedule.add(buyer)
 
     def step(self):
         self.schedule.step()
--- ABM/abm/time.py
+++ ABM/abm/time.py
@@ -36,7 +36,7 @@ class BaseSchedulerMoneyModel(BaseScheduler):
         seller = self.sellers.peek()[2]
         buyer = self.buyers.peek()[2]
         seller_has_goods_left = seller.goods_left > 0
-        buyer_has_enough_money = buyer.money_left >= seller.min_price
+        buyer_has_enough_money = buyer.money_left >= buyer.max_price
 
         if seller_has_goods_left and buyer_has_enough_money:
             if seller.min_price <= buyer.max_price:
@@ -44,8 +44,8 @@ class BaseSchedulerMoneyModel(BaseScheduler):
                 buyer.seller = seller
                 seller.is_matched = True
                 buyer.is_matched = True
-                #self.sellers.pop_task()  # can consider changing to the remove_task stuff from pq
-                #self.buyers.pop_task()
+                self.sellers.get()  # can consider changing to the remove_task stuff from pq
+                self.buyers.get()
 
     def unmatch_agents(self):  # seems super inefficient though
         for agent in self.agents:
