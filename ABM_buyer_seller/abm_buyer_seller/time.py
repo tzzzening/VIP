d8edb8fc822def9e64aa415281e4671aef492c3d
chengTzeNing <09nhnccgtzening@gmail.com>
2020-07-15 12:36:20 +0800

    Change step method in `time.py`

--- ABM_buyer_seller/abm_buyer_seller/time.py
+++ ABM_buyer_seller/abm_buyer_seller/time.py
@@ -49,18 +49,38 @@ class SimultaneousActivationMoneyModel(SimultaneousActivation):
         # print('yoyoo')
         # print(self.__str__())
         # self.match_agents()
-        for agent in self.agent_buffer(shuffled=False):
-            # maybe can come back and check this buffer thing and try to change it to a WasteAgent
-            agent.step()
-            if isinstance(agent, Seller):
-                self.total_waste_produced += agent.waste_left
-                self.total_cost_without_trading_seller += \
-                    agent.waste_left * agent.cost_per_unit_waste_disposed
-                if agent.is_matched:
-                    self.set_trade_quantity(agent)
+        # for agent in self.agent_buffer(shuffled=False):
+        #     # maybe can come back and check this buffer thing and try to change it to a WasteAgent
+        #     agent.step()
+        #     if isinstance(agent, Seller):
+        #         self.total_waste_produced += agent.waste_left
+        #         self.total_cost_without_trading_seller += \
+        #             agent.waste_left * agent.cost_per_unit_waste_disposed
+        #         if agent.is_matched:
+        #             self.set_trade_quantity(agent)
+        #
+        # for agent in self.agent_buffer(shuffled=False):
+        #     agent.advance()
+        # here
+        for i in range(len(self.sellers)):
+            seller = self.get_seller_from_list(i)
+            seller.step()
+            self.total_waste_produced += seller.waste_left
+            self.total_cost_without_trading_seller += \
+                seller.waste_left * seller.cost_per_unit_waste_disposed
+            if seller.is_matched:
+                self.set_trade_quantity(seller)
+
+        for i in range(len(self.buyers)):
+            buyer = self.get_buyer_from_list(i)
+            buyer.step()
 
         for agent in self.agent_buffer(shuffled=False):
             agent.advance()
+
+
+
+
         self.steps += 1
         self.time += 1
 
@@ -73,6 +93,12 @@ class SimultaneousActivationMoneyModel(SimultaneousActivation):
         buyer.trade_quantity = trade_quantity
         self.total_waste_traded += trade_quantity
         return
+
+    def get_seller_from_list(self, index) -> Seller:
+        return self.sellers[index][2]
+
+    def get_buyer_from_list(self, index) -> Buyer:
+        return self.buyers[index][2]
 
 
 
